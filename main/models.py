63bdd19c98315cb72cc68a721afc9720e8fe1b8c
tajreee <tajrianhaar@gmail.com>
2023-09-10 22:18:35 +0700

    update models.py

--- main/models.py
+++ main/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    amount = models.IntegerField()
+    grade = models.CharField(max_length=255, default=None)
+    category = models.CharField(max_length=255, default=None)
+    amount = models.IntegerField(default=1)
     description = models.TextField()
