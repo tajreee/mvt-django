0c79c03d16faf0019908c617afbf8f6b917042db
tajreee <tajrianhaar@gmail.com>
2023-09-11 11:32:16 +0700

    update models.py & main.html

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'title_store': 'ngothon',
-        'class': 'PBP A'
+        'title_store': 'NgopY',
+        'name' : 'Tajri Mintahtihal Anhaar',
+        'class': 'PBP F'
     }
 
     return render(request, "main.html", context)
